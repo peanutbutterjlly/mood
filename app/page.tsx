14d4bb23781963b59ea17a18bdaf8fcf5d726fb9
Steve <steverios_sr@yahoo.com>
2024-05-23 17:59:10 -0500

    created an 'award winning' landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <div className="w-screen h-screen bg-zinc-950 flex justify-center items-center text-slate-50">
+      <div className="w-full max-w-2xl">
+        <h1 className="text-6xl text-balance">The Best Journal App, Period.</h1>
+        <p className="text-2xl text-slate-50/60 my-4 text-pretty">
+          The is the best app for tracking your mood throughout your life. All
+          you have to do is be honest.
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              Get Started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
